d57715bcca6a8079a9d1b5c47a4d2e4006398280
JeongHanO <ojh980228@gmail.com>
2023-04-04 17:09:56 +0900

    neverTailwind

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-print("hell")
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbjungle
+
+## HTML을 주는 부분
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/login')
+def login():
+   return render_template('login.html')
+
+@app.route('/join')
+def join():
+   return render_template('join.html')
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5000,debug=True)
