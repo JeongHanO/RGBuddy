250ee2cb0b3a4205c2dce5fece0950cd1c88c10c
seoyoung059 <asdf13531@yonsei.ac.kr>
2023-04-05 14:37:46 +0900

    merge - app

--- app.py
+++ app.py
@@ -7,36 +7,116 @@ client = MongoClient('localhost', 27017)
 db = client.dbjungle
 
 import hashlib, jwt, calendar
-from datetime import datetime
+from datetime import datetime, timedelta
 
 KEYCODE = 'RGBuddy_key'
+
+def tokenCheck():
+   token_receive = request.cookies.get('mytoken')
+   try:
+      payload = jwt.decode(token_receive, KEYCODE, algorithms=['HS256'])
+      user_info = db.users.find_one({"id": payload['id']})
+   except jwt.ExpiredSignatureError:
+      return [False, redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))]
+   except jwt.exceptions.DecodeError:
+      return [False, redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))]
+   return [ True, user_info]
+
+
 
 ## HTML을 주는 부분
 @app.route('/')
 def home():
-   # return render_template('index.html')
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, KEYCODE, algorithms=['HS256'])
-        user_info = db.users.find_one({"id": payload['id']})
-        return render_template('calendar.html')
-    except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
-    except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+   login = tokenCheck()
+   if login[0]==True:
+      return render_template('index.html')
+   else:
+      return login[1]
 
 @app.route('/login')
-def login():
-   return render_template('login_.html')
+def login():   
+   login = tokenCheck()
+   if login[0]==True:
+      return redirect(url_for("calendar", msg="로그인 되어있습니다."))
+   else:
+      return render_template('login_.html')
 
-@app.route('/join')
-def join():
-   return render_template('join.html')
+@app.route('/signup')
+def signup():   
+   login = tokenCheck()
+   if login[0]==True:
+      return redirect(url_for("calendar", msg="로그인 되어있습니다."))
+   else:
+      return render_template('join_.html')
+
+
+
+@app.route('/calendar')
+def calendar():
+    #(추가필요) 로그인이 되어있는지 확인해야 함. 
+   login = tokenCheck()
+   if login[0]==True:
+      now = datetime.now()
+      year = now.year
+      month = now.month
+      day = now.day
+      weekday = now.weekday()
+
+      weekrange=[]
+      for i in range(14):
+         weekrange.append((now-timedelta(days=weekday-i)).day)
+      return render_template('calendar_.html', month=month, day=day, weekday=weekday, weekrange=weekrange)
+   else:
+      return login[1]
+
+
+@app.route('/matching', methods = ['GET'])
+def matching():
+
+   login = tokenCheck()
+   if login[0]==True:
+      teamColor = login[1]['team']
+    
+      date = request.args.get('date')
+
+      query = {'date': date}
+      
+      data = list(db.dates.find(query))
+      
+      if not data:
+         redCount = 0
+         blueCount = 0
+         greenCount=0
+      else:
+         redCount = len(data[0]['red'])
+         blueCount = len(data[0]['blue'])
+         greenCount = len(data[0]['green'])
+      
+      enMonth = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ]
+      month, day = map(int, date.split("/"))
+      enDate = enMonth[month]+" "+str(day)
+      
+
+      return render_template('matching.html', redCount=redCount, blueCount=blueCount, greenCount=greenCount, teamColor=teamColor, date=enDate)
+   else:
+      return login[1]
+
+@app.route('/matchLog')
+def matchLog():
+   login = tokenCheck()
+   if login[0]==True:
+      userInfo = login[1]
+      
+      userInfo['id']
+      return render_template('matchLog.html', userName=userInfo['name'], waitDates=['4/8', '4/15'])
+      
+   else:
+      return login[1]
 
 # API 역할을 하는 부분
 
-@app.route('/signup', methods= ['POST'])
-def signup():
+@app.route('/api/signup', methods= ['POST'])
+def api_signup():
    id_receive = request.form['id_give']  
    pw_receive = request.form['pw_give']
    name_receive = request.form['name_give']
@@ -66,7 +146,7 @@ def api_login():
    if result is not None:
       payload = {
          'id': id_receive,
-         'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=5)
+         'exp': datetime.utcnow() + timedelta(minutes=5)
       }
 
       token = jwt.encode(payload, KEYCODE, algorithm='HS256')
@@ -74,49 +154,35 @@ def api_login():
    else:
       return jsonify({'result': 'fail', 'msg': '아이디 또는 비밀번호가 일치하지 않습니다.'}) 
 
-@app.route('/calendar')
-def calendar():
-    #(추가필요) 로그인이 되어있는지 확인해야 함.
-    now = datetime.now()
-    year = now.year
-    month = now.month
-    day = now.day
-    weekday = now.weekday()
-    
-    return render_template('calendar.html', year=year, month=month, day=day, weekday=weekday)
-    
-#GET으로 날짜값을 넘겨준다음, 그 날짜에 해당하는 
-@app.route('/matching', methods = ['GET'])
-def matching():
+@app.route('/api/apply', methods= ['POST'])
+def apply():
 
-    #(추가필요) 로그인이 되어있는지 확인해야 함.
-    
-    #(수정필요) 사용자의 팀을 불러서 저장
-    teamColor ='red' # 임시저장
-    
-    
-    date = request.args.get('date')
-    
-    #받아온 date로 db에서 각 팀에 저장된 사람의 수를 불러옴
-    query = {'date': date}
-    
-    data = list(db.dates.find(query))
-    
-    if not data:
-        redCount = 0
-        blueCount = 0
-        greenCount=0
+    login = tokenCheck()
+    if login[0]==True:
+        userInfo = login[1]
     else:
-        redCount = len(data[0]['red'])
-        blueCount = len(data[0]['blue'])
-        greenCount = len(data[0]['green'])
+        return login[1]
     
+    date_receive = request.form['date_give']
+    month, day = map(str, date_receive.split(" "))
+    month ='January'
     enMonth = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ]
-    month, day = map(int, date.split("/"))
-    enDate = enMonth[month]+" "+str(day)
-   
+    month_number = enMonth.index(month)
+    date_formatted = str(month_number)+'/'+str(day)
 
-    return render_template('matching.html', redCount=redCount, blueCount=blueCount, greenCount=greenCount, teamColor=teamColor, date=enDate)
+    teamColor = userInfo['team']
+    userId = userInfo['id']
+    
+   #  query = {"date": date_formatted}
+   #  new_value = {"$push": {teamColor: userId}}
+    # 데이터 업데이트
+   #  db.dates.update_one(query, new_value)
+
+    date_col=db.dates.find_one({'date':date_formatted})
+    date_col.update  
+
+
+    return jsonify({'result': 'success'})
 
 
 if __name__ == '__main__':
